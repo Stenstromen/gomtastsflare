9727a886d9c3a00fd8cf9e43655b8fd73c068d5f
Stenstromen <104073740+Stenstromen@users.noreply.github.com>
2023-03-31 00:56:19 +0200

    Added basic structure

--- cmd/create.go
+++ cmd/create.go
@@ -15,7 +15,14 @@ var createCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(createCmd)
-	createCmd.AddCommand(resource.ResourceCreateCmd)
 	createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	createCmd.Flags().StringP("name", "n", "", "Resource Name")
+	createCmd.Flags().StringP("username", "u", "", "Resource Username")
+	createCmd.Flags().String("uri", "", "Resource URI")
+	createCmd.Flags().StringP("password", "p", "", "Resource Password")
+	createCmd.Flags().StringP("description", "d", "", "Resource Description")
+	createCmd.Flags().StringP("folderParentID", "f", "", "Folder in which to create the Resource")
+
+	createCmd.MarkFlagRequired("name")
 
 }
--- resource/create.go
+++ resource/create.go
@@ -2,15 +2,82 @@ package resource
 
 import (
 	"fmt"
+	"os"
+
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
 
-var ResourceCreateCmd = &cobra.Command{
-	RunE: ResourceCreate,
+func ResourceCreate(cmd *cobra.Command, args []string) error {
+	var bearer = "Bearer " + os.Getenv("TOKEN")
+	name, err := cmd.Flags().GetString("name")
+	if err != nil {
+		return err
+	}
+	fmt.Println(name)
+	fmt.Println("Create Cloudflare Records here")
+	fmt.Println(bearer)
+	return nil
 }
 
-func ResourceCreate(cmd *cobra.Command, args []string) error {
-	fmt.Println("Create Cloudflare Records here")
-	return nil
+func postToCloudflare(portandprotocol string, nameanddomain string, postBody string) {
+	url := "https://api.cloudflare.com/client/v4/zones"
+	var bearer = "Bearer " + os.Getenv("TOKEN")
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	req.Header.Add("Authorization", bearer)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error while reading the response bytes:", err)
+	}
+	var res Res
+	if err := json.Unmarshal(body, &res); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	var did string
+
+	for i := range res.Result {
+		if nameanddomain == res.Result[i].Name {
+			did = res.Result[i].ID
+		}
+	}
+
+	posturl := "https://api.cloudflare.com/client/v4/zones/" + did + "/dns_records"
+	var jsonStr = []byte(postBody)
+	req2, err2 := http.NewRequest("POST", posturl, bytes.NewBuffer(jsonStr))
+	if err2 != nil {
+		log.Println(err2)
+		os.Exit(1)
+	}
+
+	req2.Header.Set("Content-Type", "application/json")
+	req2.Header.Add("Authorization", bearer)
+
+	client2 := &http.Client{}
+	resp2, err2 := client2.Do(req2)
+	if err2 != nil {
+		log.Println(err2)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	log.Println("Cloudflare Response Status:", resp2.Status)
 }
